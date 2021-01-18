87ad2986cf2a49c318a970c17a34476d6c83b765
urain39 <urain39@qq.com>
2021-01-19 01:32:04 +0800

    修复可能重复的问题

--- astr/command.py
+++ astr/command.py
@@ -24,11 +24,12 @@ def extract(config: ConfigParser) -> None:
     extract_dir = work_dir / dir
     cache_dir = extract_dir / _CONST_ASTRCACHE
 
+    database: List[str] = []
     for i in work_dir.glob(inc):
         if i.is_file():
             text = i.read_text(encoding=enc)
 
-            cache_file = (cache_dir / (i.name + '.json'))
+            cache_file = (cache_dir / (str(i) + '.json'))
             # 注意：这里cache文件可能有父目录
             cache_file.parent.mkdir(parents=True, exist_ok=True)
 
@@ -38,12 +39,17 @@ def extract(config: ConfigParser) -> None:
             if stringList:
                 cache_file.write_text(json.dumps(tokens), encoding=enc)
 
-                text_file = extract_dir / (i.name + '.txt')
+                text_file = extract_dir / (str(i) + '.txt')
+                text_file.parent.mkdir(parents=True, exist_ok=True)
                 text_file.write_text(
                     # 排序后方便翻译
                     '\n\n\n'.join(sorted(stringList)), encoding=enc
                 )
 
+                database.append(str(i))
+
+            database_file = extract_dir / 'database.json'
+            database_file.write_text(json.dumps(database))
 
 def inject(config: ConfigParser) -> None:
     inc = config.get(_CONST_MAIN, _CONST_INCLUDE)
@@ -54,23 +60,25 @@ def inject(config: ConfigParser) -> None:
     extract_dir = work_dir / dir
     cache_dir = extract_dir / _CONST_ASTRCACHE
 
-    for i in work_dir.glob(inc):
-        if i.is_file():
-            text_file = extract_dir / (i.name + '.txt')
-            text = text_file.read_text(encoding=enc)
+    database_file = extract_dir / 'database.json'
+    database = json.loads(database_file.read_text())
 
-            unmatchedList, dictionary = parseDictionary(text)
+    for i in database:
+        text_file = extract_dir / (str(i) + '.txt')
+        text = text_file.read_text(encoding=enc)
 
-            if unmatchedList:
-                print('Untranslated string detected!')
+        unmatchedList, dictionary = parseDictionary(text)
+
+        if unmatchedList:
+            print('WARNING: untranslated string detected!')
             
-                for i_ in unmatchedList:
-                    print(f'    unmatched: {i_}')
+            for t in unmatchedList:
+                print(f'    unmatched: {t}')
 
-            cache_file = (cache_dir / (i.name + '.json'))
+        cache_file = (cache_dir / (str(i) + '.json'))
 
-            text = _inject(json.loads(cache_file.read_text()), dictionary)
-            i.write_text(text, encoding=enc)
+        text = _inject(json.loads(cache_file.read_text()), dictionary)
+        Path(i).write_text(text, encoding=enc)
 
 
 def execute(cmd: str, config: ConfigParser) -> None:
