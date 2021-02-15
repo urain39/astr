3ba64d8b75f809766c637fa0a80714c24456c83c
urain39 <urain39@qq.com>
2021-02-15 15:31:37 +0800

    添加单引号支持。

--- astr/extractor.py
+++ astr/extractor.py
@@ -1,12 +1,17 @@
 import re
+import json
 
 from typing import Dict, List, Tuple
 from .annotations import Token
 from .exceptions import ExtractorError
 
+
+
 # 用于提取，组1是匹配到的文本，支持“\uUUUU”，“\xXX”
-_RE_STRING = re.compile(
-    r'"((?:[^"\\]|\\[^ux]|\\u[\dA-Fa-f]{4}|\\x[\dA-Fa-f]{2})*)"', re.MULTILINE)
+_STR_RE_STRING = r'"((?:[^"\\]|\\[^ux]|\\u[\dA-Fa-f]{4}|\\x[\dA-Fa-f]{2})*)"'
+
+_RE_STRING = re.compile(_STR_RE_STRING, re.MULTILINE)
+_RE_STRING2 = re.compile(_STR_RE_STRING.replace('"', '\''), re.MULTILINE)
 # 用于判断，这里的文本是全部匹配
 _RE_WHITESPACE = re.compile(r'^(\s+)?$')
 _RE_ASCII = re.compile(r'^([\x00-\x7f]+)$')
@@ -16,11 +21,17 @@ TYPE_REFERENCE = 1
 TYPE_OTHERS = 2
 
 
+def normalize_quote(source: str) -> str:
+    return _RE_STRING2.sub(lambda match: f'{json.dumps(match.group(1))}', source)
+
+
 def extract(source: str) -> Tuple[List[str], List[Token]]:
     tokens: List[Token] = []
     string_id: int = 0
     string_map: Dict[str, int] = {}  # [string] -> string_id
     string_list: List[str] = []  # [string_id] -> string
+
+    source = normalize_quote(source)
 
     s = ''
     i = 0  # 当前
@@ -51,7 +62,7 @@ def extract(source: str) -> Tuple[List[str], List[Token]]:
             raise ExtractorError(f'Unexpected char \'{source[i]}\'')
 
         # 引号内部
-        s = m.group(1)
+        s = eval(m.group(0))
         i = m.end(0)
 
         # 过滤掉空白符和ASCII码
